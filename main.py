e8ca910fa569218da0449004cd9c6671fd049333
gott <gogottu1221@gmail.com>
2024-12-08 19:55:02 +0900

    OptionUpdate関数を追加

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import secrets
 import string
 import pyperclip
+from typing import Union
 
-def yes_or_no(query:str): 
+def yes_or_no(query:str):
     while True:
         match input(query).lower():
             case "y" | "ye" | "yes":
@@ -11,11 +12,11 @@ def yes_or_no(query:str):
                 return False
 
 class Options:
-    Uppercase: bool
-    Lowercase: bool
-    digits: bool
-    symbol: bool
-    length: int
+    Uppercase:bool
+    Lowercase:bool
+    digits:bool
+    symbol:bool
+    length:int
 
 class strings:
     UPPERCASE=string.ascii_uppercase 
@@ -25,10 +26,10 @@ class strings:
 
 class Generator:
     def generate():
-        output: str=""
-        for _ in range(Options.length): 
-            output:str =+ secrets.choice(Generator.choiceall(Options.Uppercase,Options.Lowercase,Options.digits,Options.symbol))
-            
+        output:str=""
+        for _ in range(Options.length):
+            z = secrets.choice(Generator.choiceall(Options.Uppercase,Options.Lowercase,Options.digits,Options.symbol))
+            output = output + z
         return output
     
     def question():
@@ -38,31 +39,50 @@ class Generator:
                 Options.Uppercase=yes_or_no("大文字含む?(y/n)")
                 Options.Lowercase=yes_or_no("小文字含む?(y/n)")
                 Options.digits=yes_or_no("数字含む?(y/n)")
-                Options.symbol=yes_or_no("記号含む?(y/n)") 
+                Options.symbol=yes_or_no("記号含む?(y/n)")
                 x=int(input("長さどれくらい?(1以上の整数)"))
                 if x>=1:
                     Options.length=x
                 else:
-                    raise ValueError() 
+                    raise ValueError()
 
-                if Options.Uppercase | Options.Lowercase | Options.digits | Options.symbol: 
+                if Options.Uppercase | Options.Lowercase | Options.digits | Options.symbol:
                     break 
-                else: 
+                else:
                     print("このジェネレータに対して少なすぎる引数が入力されています。")
-            except ValueError: 
+            except ValueError:
                 print("それは1以上の整数ではありません。")
 
-    def choiceall(Uppercase:bool,lowercase:bool,digits:bool,symbol:bool):
-        x: str = ""
-        if Uppercase: x+=str(strings.UPPERCASE)
-        if lowercase: x+=str(strings.LOWERCASE)
-        if digits: x+=str(strings.DIGITS)
-        if symbol: x+=str(strings.SYMBOL)
+    def choiceall(Uppercase:bool,lowercase:bool,digits:bool,symbol:bool)-> str:
+        x:str = ""
+        if Uppercase:x+=str(strings.UPPERCASE)
+        if lowercase:x+=str(strings.LOWERCASE)
+        if digits:x+=str(strings.DIGITS)
+        if symbol:x+=str(strings.SYMBOL)
         return x 
+    
+    def OptionUpdate(type:Options, content:Union[bool,int]):
+        match type:
+            case Options.Uppercase:
+                Options.Uppercase=content
+                return
+            case Options.Lowercase:
+                Options.Lowercase=content
+                return
+            case Options.digits:
+                Options.digits=content
+                return
+            case Options.symbol:
+                Options.symbol=content
+                return
+            case Options.length:
+                Options.length=content
+                return
 
-if __name__=="__main__": 
-    Generator.question() 
-    output_=Generator.generate() 
-    print(output_) 
-    pyperclip.copy(output_) 
-    print("Copied!") 
+if __name__=="__main__":
+    Generator.question()
+    Generator.OptionUpdate(Options.digits)
+    output_=Generator.generate()
+    print(output_)
+    pyperclip.copy(output_)
+    print("Copied!")
